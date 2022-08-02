ac1873abc824c104e2cd9a60fec17ac7e5b2afa2
tolotriniaina <tramasihenintsoa@ingenosya.mg>
2022-08-02 11:28:32 +0300

    Correction sidebar

--- lib/layouts/admin-layout/sidenav/sidenav.component.d.ts
+++ lib/layouts/admin-layout/sidenav/sidenav.component.d.ts
@@ -1,10 +1,11 @@
-import { OnInit } from '@angular/core';
+import { EventEmitter, OnInit } from '@angular/core';
 import * as i0 from "@angular/core";
 export interface itemMenus {
     router: string;
     icone: string;
     name: string;
     asSubnav: SubNavMenus[];
+    isModal: boolean;
 }
 export interface SubNavMenus {
     router: string;
@@ -14,8 +15,11 @@ export interface SubNavMenus {
 export declare class SidenavComponent implements OnInit {
     logo: string;
     GetItemMenus: itemMenus[];
+    isModal: boolean;
+    click_menu: EventEmitter<any>;
     constructor();
     ngOnInit(): void;
+    prop_menu(event: boolean, router: string): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<SidenavComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<SidenavComponent, "igy-sidenav", never, { "logo": "logo"; "GetItemMenus": "GetItemMenus"; }, {}, never, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<SidenavComponent, "igy-sidenav", never, { "logo": "logo"; "GetItemMenus": "GetItemMenus"; "isModal": "isModal"; }, { "click_menu": "click_menu"; }, never, never>;
 }
